6522f54e3530364c6141269fd10261ae754d7459
Vee <931418134@qq.com>
2018-08-20 12:34:40 +0800

    新媒体部完成

---
+++ resources/assets/js/views/department/Media.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Media"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/views/department/video.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "video"
+    }
+</script>
+
+<style scoped>
+
+</style>
